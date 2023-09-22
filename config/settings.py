f593fefad24ced24ba0db7453bffd8c535a084fe
Irina Fok <fokira@mail.ru>
2023-09-22 21:26:27 +0300

    Create model file

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "files",
+    "file",
     "rest_framework",
 ]
 
--- file/models.py
+++ file/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    """Модель для файла."""
+
+    file = models.FileField(upload_to="uploads/", max_length=256)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    processed = models.BooleanField(default=False)
